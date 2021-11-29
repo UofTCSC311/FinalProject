48c24b5df6fca6cbc1876a6bb712455fc7bb2d09
Syed Ahmed <76264982+SyedTawsifAhmed@users.noreply.github.com>
2021-11-28 20:50:11 -0500

    Update ensemble.py

--- part_a/ensemble.py
+++ part_a/ensemble.py
@@ -1,4 +1,5 @@
 # TODO: complete this file.
+import random
 from utils import *
 import item_response
 import neural_network
@@ -6,26 +7,31 @@ import knn
 import matrix_factorization
 
 
-def boostrap_matrix(matrix, m):
+def bootstrap_matrix(matrix):
     """ boostrap samples for a sparse matrix
     :param matrix: 2D sparse matrix
-    :param m: int
     :return: 2D sparse matrix
     """
     # TODO
     pass
 
 
-def boostrap(data, m):
+def bootstrap(data):
     """ boostrap samples from a dataset
     :param data: A dictionary {user_id: list, question_id: list,
     is_correct: list}
-    :param m: int
     :return: A dictionary {user_id: list, question_id: list,
     is_correct: list}
     """
     # TODO
-    pass
+    length = len(data["question_id"])
+    new_data = {"user_id": [], "question_id": [], "is_correct": []}
+    for i in range(length):
+        i = np.random.choice(length, 1)[0]
+        new_data["user_id"].append(data["user_id"][i])
+        new_data["question_id"].append(data["question_id"][i])
+        new_data["is_correct"].append(data["is_correct"][i])
+    return new_data
 
 
 def main():
@@ -38,15 +44,22 @@ def main():
     model2 = item_response.irt
     model3 = neural_network.train
 
-    train_data1 = np.random.sample()
-    val_data1 = None
-    test_data1 = None
-    train_data2 = None
-    val_data2 = None
-    test_data2 = None
-    train_data3 = None
-    val_data3 = None
-    test_data3 = None
+    # temporary set seed
+    random.seed(1)
+
+    train_data1, train_data2, train_data3 = bootstrap(train_data), \
+                                            bootstrap(train_data), \
+                                            bootstrap(train_data)
+
+    irt_lr = 0.01
+    irt_iterations = 25
+    irt_theta, irt_beta, irt_train_likelihoods, irt_valid_likelihoods, \
+    irt_train_acc_lst, irt_valid_acc_lst = item_response.irt(
+        train_data, val_data, irt_lr, irt_iterations)
+    print("Final accuracy on the validation data is:" +
+          str(item_response.evaluate(val_data, irt_theta, irt_beta)))
+    print("Final accuracy on the training data is:" +
+          str(item_response.evaluate(test_data, irt_theta, irt_beta)))
 
     pred1 = None
     pred2 = None
